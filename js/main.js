efb3ec6b53b31ea56e34649a69b0a45a14798d0b
Gustavo <gus.atanazio@gmail.com>
2024-05-31 17:43:19 -0300

    Refatorando lógica do slider

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 // Lógica do slider
-let counter = 0;
-const slides = document.querySelectorAll('.slide');
+function slider(slideSelector, slidesContainerSelector, time) {
+    let counter = 0;
+    const slidesElements = document.querySelectorAll(slideSelector);
 
-setInterval(() => {
-    counter++;
-    if (counter >= slides.length) counter = 0;
+    setInterval(() => {
+        counter++;
+        if (counter >= slidesElements.length) counter = 0;
 
-    document.querySelector('.slides').style.transform = `translateX(-${counter * 100}%)`;
-}, 5000);
+        document.querySelector(slidesContainerSelector).style.transform = `translateX(-${counter * 100}%)`;
+    }, time);
+}
+
+slider('.slide', '.slides', 5000);
+
+// Slider da seção 2
+slider('.slide_section', '.slides_section', 4000);
